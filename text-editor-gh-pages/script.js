d0908f8ab8adf0dbeb6d98aa522ec8ae68bc636e
Rohithshan <53473402+Rohithshan@users.noreply.github.com>
2020-07-17 19:05:49 +0530

    added theme

--- text-editor-gh-pages/script.js
+++ text-editor-gh-pages/script.js
@@ -1,55 +1,29 @@
-/**
- * TODO: Update the text in the "Formatted Text" section as a user types in the textarea
- * TODO TOGETHER: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
- * TODO: Add an .underline class to "Formatted Text" when Underline button is clicked
- * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked
- */
-
-
 window.onload = function() {
   document.getElementById('text-input').focus();
   document.getElementById('text-input').select();
-
+  document.getElementById('dark-theme').addEventListener('click', darkTheme);
 }
-/**
- * Update the output text as a user types in the textarea
- * HINT: Use the onkeydown function inside HTML
- */
+
+darkTheme = () => {
+  document.querySelector('body').classList.toggle('dark');
+}
+
 function updateText(){
-  // CODE GOES HERE
   let text = document.getElementById('text-input').value;
   document.getElementById('text-output').innerText = text;
 }
 
-/**
- * Toggle the bold class for the output text
- * HINT: Use the onclick function insite HTML
- * HINT: Look into using this keyword
- * HINT: Use the classList property
- * HINT: Toggle .active class for the button
- */
 function makeBold(elem){ 
-  //CODE GOES HERE
   elem.classList.toggle('active');
   document.getElementById('text-output').classList.toggle('bold');
 }
-
-/**
- * Toggle the italic class for the output text
- */
 
 function makeItalic(elem){
   elem.classList.toggle('active');
   document.getElementById('text-output').classList.toggle('italic');
 }
-/**
- * Toggle the underline class for the output text
- * HINT: Toggle the .active class for the button
- * HINT: Use the classList property
- * HINT: Use contains, remove, and add functions
- */
+
 function makeUnderline(elem){
-  //CODE GOES HERE
   elem.classList.toggle('active');
   let formattedText = document.getElementById('text-output');
   
@@ -60,15 +34,7 @@ function makeUnderline(elem){
   }
 }
 
-/**
- * Toggle the style textAlign attribute
- * Toggle the active state for the align butttons
- * HINT: Use the style property of the element
- * HINT: Make sure to untoggle the active state for all other align buttons
- */
-
 function alignText(elem, alignType) {
-  // CODE GOES HERE
   document.getElementById('text-output').style.textAlign = alignType;
   let buttonList = document.getElementsByClassName('align');
   for (let i=0; i < buttonList.length; i++) {
@@ -105,8 +71,27 @@ function textTospeech(){
 function Changecolor() {
   let color = document.getElementById('color-palette').value;
   document.getElementById('text-output').style.color = color;
-  document.getElementById('header-title').style.color = color;
+  // document.getElementById('header-title').style.color = color;
 }
 
+
+function activateCaps(elem){ 
+  elem.classList.toggle('active');
+  document.getElementById('text-output').classList.toggle('caps');
+}
+
+// side Nav
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "15%";
+  document.getElementById("container").style.gridTemplateColumns = "15% 75%";
+    
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("container").style.gridTemplateColumns = "3% 97%";
+
+}
 
 
